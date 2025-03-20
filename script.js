a48d09af594f1e2eb2d252de8cb3760a3dd309c9
italoluc <italoluc.dev@gmail.com>
2025-03-20 10:05:05 -0300

    Reorganizando arquivos e alterando o visual das páginas

---
+++ script.js
@@ -0,0 +1,13 @@
+// Suaviza o scroll da página
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
